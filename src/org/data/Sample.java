b0fffeba20e7564281090d9ecd15e3abb247aa39
magiselvi81 <magiselvi81@gmail.com>
2022-08-29 17:01:30 +0530

    Lines of code added

---
+++ src/org/data/Sample.java
@@ -0,0 +1,12 @@
+package org.data;
+
+public class Sample {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Mahi.......");
+		
+		System.out.println("Sucess.......");
+	}
+
+}
