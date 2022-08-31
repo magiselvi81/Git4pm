1fc4179b40c56364f61f0406fe5c3c2cf5b5d9f9
magiselvi81 <magiselvi81@gmail.com>
2022-08-31 21:59:26 +0530

    Code were added by Magi

--- src/org/data/Sample.java
+++ src/org/data/Sample.java
@@ -2,7 +2,11 @@ package org.data;
 
 public class Sample {
 	
+	private void m1() {
+		System.out.println("Magi added code..............");
+	}
 	public static void main(String[] args) {
+		
 		
 		System.out.println("Mahi.......");
 		
